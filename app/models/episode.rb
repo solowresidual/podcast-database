971bd7e8fb5981e13b5170ec7651fd224047dd5e
Rebecca Ross <b.laurenross@gmail.com>
2020-11-24 15:42:09 +0000

    fixes associations

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,5 @@
 class Episode < ApplicationRecord
   belongs_to :podcast
   has_many :reviews
+  has_many :favorite
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :reviews, :favorites
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :reviews
+  has_many :favorites
 end
