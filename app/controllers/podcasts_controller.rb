4ba9b8fe0313153523b15977f568416dbceabcf5
Alex Price <alexprice009@gmail.com>
2020-11-25 16:12:19 +0000

    Search working

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,6 +1,15 @@
 class PodcastsController < ApplicationController
+  skip_before_action :authenticate_user! , only: :index
   def show
     @podcast = Podcast.find(params[:id])
     @episodes = @podcast.episodes
+  end
+
+  def index
+    if params[ :query ]
+      @podcasts = Podcast.search_by_name_and_description(params[ :query])
+    else
+      @podcasts = Podcast.all
+    end
   end
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,3 +1,9 @@
 class Podcast < ApplicationRecord
   has_many :episodes
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description, :category ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
