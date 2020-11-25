1db1a0366b47b5cf0747c9f4a8b19237421db772
Ceinwen Michael <ceinwen.michael@gmail.com>
2020-11-25 14:52:14 +0000

    podcast index done

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,6 +1,12 @@
 class PodcastsController < ApplicationController
+
+  def index
+    @podcasts = Podcast.all
+  end
+
   def show
     @podcast = Podcast.find(params[:id])
     @episodes = @podcast.episodes
   end
+
 end
