9e438a444425429e2331c2a812fe9433c3a2f95f
Rebecca Ross <b.laurenross@gmail.com>
2020-11-25 14:44:09 +0000

    Adds functioning review form to episode show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   def create
     @user = current_user
     @review = Review.new(review_params)
-    @episode = Episode.find(params([:episode_id]))
+    @episode = Episode.find(params[:episode_id])
 
     @review.user = @user
     @review.episode = @episode
