dd7d263031cca09f64f18e296bfc451e28d375ce
John Joubert <john@robotdwarf.com>
2020-11-25 17:45:36 +0000

    Create average rating for episode

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :episode
+  after_save :update_average_rating
+
+  private
+
+  def update_average_rating
+    episode = self.episode
+    average_rating = episode.reviews.average(:rating).round
+    episode.update_attributes(average_rating: average_rating)
+  end
 end
