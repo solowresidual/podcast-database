0500d965218db9f192265de424cd9786e1d1165d
John Joubert <john@robotdwarf.com>
2020-11-26 13:04:57 +0000

    Add remove favorite functionality

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,11 +1,11 @@
 class EpisodesController < ApplicationController
   before_action :set_episode, only: [:show]
-  skip_before_action :authenticate_user!, only: [ :show ]
+  skip_before_action :authenticate_user!, only: [:show]
 
   def show
     @reviews = @episode.reviews
     @review = Review.new
-    @favorite = Favorite.new
+    @favorite = Favorite.find_by(episode: @episode)
   end
 
   private
