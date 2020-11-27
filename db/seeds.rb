8df60d7dfbd8341cc9c81d0e9889c390094f33d2
John Joubert <john@robotdwarf.com>
2020-11-27 11:58:03 +0000

    Create manual seed file and save backup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
 puts 'Clearing the DB'
@@ -26,6 +19,28 @@ puts 'users destroyed'
 
 puts 'Starting seed file...'
 
+# ----------------Start Creating Podcast -----------------
+podcast1 = Podcast.create!(
+  name: "Test Podcast",
+  description: "description",
+  category: "Something"
+)
+# ----------------Finish Creating Podcast -----------------
+
+# ----------------Start Creating Episode -----------------
+puts "podcast created: #{podcast1.name}" # Should match the Podcast variable name you picked above
+
+episode1 = Episode.create!(
+  name: "Test Episode",
+  description: "Lorem ipsum dolor sit amet, vidit graecis disputationi ei ius, ea voluptaria sadipscing eloquentiam vim. Cu his eros insolens. Eu eripuit singulis scriptorem eos. Ea nonumes iudicabit pri, wisi tempor recusabo ex vis, habeo nostrud eam cu. Vix ponderum pertinax complectitur ne, dolor scribentur id ius, ut mel fugit consetetur.
+
+    Eos et simul assentior. Gubergren euripidis est ad. At dissentias sadipscing signiferumque mel. Impetus mentitum facilisi ius et, no sumo summo mollis mea. Pri augue omnes ut, sit accusam persequeris ex, no postulant partiendo sit. Facete iuvaret adipisci sit id, eos eu agam iudicabit philosophia. Ei eum volumus adversarium.",
+  podcast: podcast1 # Should match the Podcast variable name you picked above
+)
+
+puts "episode created: #{episode1.name}" # Should match the Episode variable you picked above
+# ----------------Finish Creating Episode -----------------
+
 10.times do
   user = User.create(
     email: Faker::Internet.email,
@@ -34,38 +49,19 @@ puts 'Starting seed file...'
 
   puts "user created: #{user.email}"
 
-  podcast = Podcast.create(
-    name: Faker::Music.album,
-    description: "This is a placeholder",
-    category: Faker::Music.genre
-  )
-
-  puts "podcast created: #{podcast.name}"
-
-  episode = Episode.create!(
-    name: Faker::Music::Prince.song,
-    description: "Lorem ipsum dolor sit amet, vidit graecis disputationi ei ius, ea voluptaria sadipscing eloquentiam vim. Cu his eros insolens. Eu eripuit singulis scriptorem eos. Ea nonumes iudicabit pri, wisi tempor recusabo ex vis, habeo nostrud eam cu. Vix ponderum pertinax complectitur ne, dolor scribentur id ius, ut mel fugit consetetur.
-
-Eos et simul assentior. Gubergren euripidis est ad. At dissentias sadipscing signiferumque mel. Impetus mentitum facilisi ius et, no sumo summo mollis mea. Pri augue omnes ut, sit accusam persequeris ex, no postulant partiendo sit. Facete iuvaret adipisci sit id, eos eu agam iudicabit philosophia. Ei eum volumus adversarium.
-",
-    podcast: podcast
-  )
-
-  puts "episode created: #{episode.name}"
-
   review = Review.create(
     content: Faker::Restaurant.review,
     rating: rand(1..5),
     tag: Faker::Music.genre,
     user: user,
-    episode: episode
+    episode: Episode.all.sample
   )
 
   puts "review created: #{review.id}"
 
   favorite = Favorite.create(
     user: user,
-    episode: episode,
+    episode: Episode.all.sample,
     finished: [false, true].sample
   )
 
