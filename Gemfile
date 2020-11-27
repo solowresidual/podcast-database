c96ca1ae1f992b0b5b7bcacfb45d59fbfa06806a
John Joubert <john@robotdwarf.com>
2020-11-27 22:54:06 +0000

    Add ability to sign in / sign up with Spotify

--- Gemfile
+++ Gemfile
@@ -36,12 +36,15 @@ gem 'cloudinary', '~> 1.16.0'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
+gem 'omniauth'
+gem 'omniauth-spotify'
 
 gem 'pg_search', '~> 2.3.0'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
+
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
   gem 'dotenv-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[spotify]
   has_many :reviews
   has_many :favorites
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.avatar = auth.info.image
+      user.username = auth.info.nickname
+    end
+  end
 end
